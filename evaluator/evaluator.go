1f3ff52799883af3b8efa26bb4f6c831eaf1f682
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-01-04 23:54:05 +0900

    support if expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -17,6 +17,12 @@ func Eval(node ast.Node) object.Object {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
+
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
@@ -46,6 +52,31 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
